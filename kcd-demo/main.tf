bf878501558302bd514e34f4d03ea9e447323ec9
Nicola Marco Decandia <nicola@decandia.eu>
2024-02-22 01:08:18 +0100

    tofu state

--- kcd-demo/main.tf
+++ kcd-demo/main.tf
@@ -5,7 +5,15 @@ terraform {
       version = ">= 2.0"
     }
   }
+  #   backend "kubernetes" {
+  #   config_path   = "~/.kube/config"
+  #   secret_suffix = "tfstate"
+  #   namespace     = "default"
+  # }
 }
+
+# kubectl get secret tfstate-default-tfstate -o jsonpath="{.data.tfstate}" | base64 -d | gzip -d -
+
 
 provider "kubernetes" {
   config_path    = "~/.kube/config"
